32dd9c1306a9ab5818954594b759a419d085cc45
lhomero <luis.gonzales@oatobservatorio.com>
2025-01-25 21:21:05 +0100

    add delete subdomain

--- src/Controller/SubdomainController.php
+++ src/Controller/SubdomainController.php
@@ -14,12 +14,18 @@ use App\Service\SubdomainService;
 final class SubdomainController extends AbstractController
 {
     private SubdomainService $subdomainService;
-    public function __construct(SubdomainService $subdomainService)
+    private Security $security;
+    public function __construct
+    (
+        SubdomainService $subdomainService, 
+        Security $security
+    )
     {
         $this->subdomainService = $subdomainService;
+        $this->security = $security;
     }
 
-    public function addSubdomain(Request $request, EntityManagerInterface $entityManager,  Security $security): Response
+    public function addSubdomain(Request $request): Response
     {
         $subdomain = new Subdomain();
 
@@ -29,7 +35,7 @@ final class SubdomainController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $subdomain = $form->getData(); 
-            $subdomain->setUser($security->getUser());
+            $subdomain->setUser($this->security->getUser());
             $subdomain->setCreatedAt(new \DateTime());
             $subdomain->setIsActive(false);
             $result = $this->subdomainService->save($subdomain);
@@ -42,6 +48,53 @@ final class SubdomainController extends AbstractController
         return $this->render('Subdomain/addSubdomain.html.twig', [
             'form' => $form->createView(), 'title' => 'Añadir subdominio'
         ]);
+    }
+    public function editSubdomain(Request $request): Response
+    {
+        $idSubdominio =  (int) $request->attributes->get('idSubdominio');
+ 
+        $subdomain = $this->subdomainService->findOneBy('id' , $idSubdominio);
+ 
+        if (!$subdomain) {
+            throw $this->createNotFoundException('Subdomain not found');
+        }
+
+        $checkUserPermissionsForThisAction = $this->security->getUser() === $subdomain->getUser();
+        if(!$checkUserPermissionsForThisAction){
+            throw $this->createNotFoundException('Not permissions for this action.');
+        }
+
+        $form = $this->createForm(SubdomainFormType::class, $subdomain);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $result = $this->subdomainService->save($form->getData());  
+
+            $this->addFlash($result ? 'success' : 'error', $result ? 'Subdominio actualizado correctamente.' : 'Error actualizando el subdominio.');
+
+            return $this->redirectToRoute('front.v1.all.subdomain');
+        }
+
+        return $this->render('Subdomain/addSubdomain.html.twig', [
+            'form' => $form->createView(),
+            'subdomain' => $subdomain,
+            'title' => 'Editar subdominio'
+        ]);
+    }
+    public function deleteSubdomain(Request $request): Response
+    {
+        $idSubdominio =  (int) $request->attributes->get('idSubdominio');
+        $subdomain = $this->subdomainService->findOneBy('id',$idSubdominio);
+
+        $checkUserPermissionsForThisAction = $this->security->getUser() === $subdomain->getUser();
+        if(!$checkUserPermissionsForThisAction){
+            throw $this->createNotFoundException('Not permissions for this action.');
+        }
+
+        $result = $this->subdomainService->remove($subdomain);
+        
+        $this->addFlash($result ? 'success' : 'error', $result ? 'Subdominio borrado correctamente.': 'Error borrando el subdominio');
+        return $this->redirectToRoute('front.v1.all.subdomain');
     }
 
     public function listSubdomain(Request $request, EntityManagerInterface $entityManager,  Security $security): Response
