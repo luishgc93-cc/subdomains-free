ec7d262e7420f16a041ec5b2924f29d364151a8f
lhomero <luis.gonzales@oatobservatorio.com>
2025-01-26 20:11:47 +0100

    add home

---
+++ src/Controller/StaticController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class StaticController extends AbstractController
+{
+    public function home(): Response
+    {
+        return $this->render('Static/home.html.twig', [
+            'title' => 'Inicio'
+        ]);
+    }
+
+}
