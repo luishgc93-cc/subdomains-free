e75df2f7517391bfb8624c4a389b3119c05539cc
lhomero <luis.gonzales@oatobservatorio.com>
2025-02-02 12:53:20 +0100

    add email service

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -41,7 +41,7 @@ final class RegistrationController extends AbstractController
 
             $this->emailVerifier->sendEmailConfirmation('front.v1.user.very.email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('noreply@serice.com', 's'))
+                    ->from(new Address('onboarding@resend.dev', 'onboarding@resend.dev'))
                     ->to((string) $user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('User/Registration/confirmation_email.html.twig')
@@ -91,7 +91,7 @@ final class RegistrationController extends AbstractController
 
         $this->emailVerifier->sendEmailConfirmation('front.v1.user.very.email', $user,
         (new TemplatedEmail())
-            ->from(new Address('noreply@serice.com', 's'))
+            ->from(new Address('onboarding@resend.dev', 'onboarding@resend.dev'))
             ->to((string) $user->getEmail())
             ->subject('Please Confirm your Email')
             ->htmlTemplate('User/Registration/confirmation_email.html.twig')
